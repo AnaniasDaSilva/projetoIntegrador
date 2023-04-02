97024839f53bc8e6221103cafcfea80e818a5e3f
teste_git <113646572+AnaniasDaSilva@users.noreply.github.com>
2023-04-01 21:19:50 -0300

    Add files via upload

---
+++ src/main/java/projetoIntregrador/hello.java
@@ -0,0 +1,10 @@
+package projetoIntregrador;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+
+	}
+
+}
---
+++ src/main/webapp/script.js
@@ -0,0 +1,3 @@
+function enviarAlerta() {
+  alert('Botão clicado!');
+}
